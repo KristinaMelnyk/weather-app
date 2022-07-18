75a93090a2cb95fd9b1be89291832d4163a3116f
KristinaMelnyk <105805696+KristinaMelnyk@users.noreply.github.com>
2022-07-18 14:07:57 +0300

    Added API forecast

--- script.js
+++ script.js
@@ -18,10 +18,12 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu"];
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+
   let forecastHTML = `<div class = "row">`;
   days.forEach(function (day) {
     forecastHTML =
@@ -48,6 +50,13 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "fc6fe7180f75f61eb4c6a27ee5f57b6b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showTemperature(response) {
   document.querySelector("h1").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
@@ -69,6 +78,8 @@ function showTemperature(response) {
       "src",
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -88,4 +99,3 @@ let form = document.querySelector("form");
 form.addEventListener("submit", handleSubmit);
 
 search("Kyiv");
-displayForecast();
